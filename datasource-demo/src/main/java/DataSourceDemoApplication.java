5ec1ff204f1de702c2c2daf4f12dbb4decb3efef
eric <wanghuanxi@gmail.com>
2020-10-26 21:40:41 +0800

    add file

---
+++ datasource-demo/src/main/java/DataSourceDemoApplication.java
@@ -0,0 +1,47 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class DataSourceDemoApplication implements CommandLineRunner {
+
+	static private Logger logger = LogManager.getLogger(DataSourceDemoApplication.class.getName());
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		showData();
+	}
+
+	private void showConnection() throws SQLException {
+		logger.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		logger.info(conn.toString());
+		conn.close();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM FOO")
+				.forEach(row -> logger.info(row.toString()));
+	}
+}
+
