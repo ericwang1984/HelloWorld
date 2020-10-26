97197817a1779d062d83c0d7fdae3fb0579b05ca
eric <wanghuanxi@gmail.com>
2020-10-26 21:49:00 +0800

    add file

--- datasource-demo/src/main/java/eric/data/datasourcedemo/DataSourceDemoApplication.java
+++ datasource-demo/src/main/java/eric/data/datasourcedemo/DataSourceDemoApplication.java
@@ -1,4 +1,5 @@
-import lombok.extern.slf4j.Slf4j;
+package eric.data.datasourcedemo;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
